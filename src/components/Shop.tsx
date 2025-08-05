1226e5d7083db923201ffe98fbf4093b3ae8ab5c
legendarylibr <legendarylibr@gmail.com>
2025-08-05 00:41:44 -0400

    Fix pink sugar asset reference in Shop.tsx

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -4,7 +4,7 @@ import MarketplaceService, { MarketplaceItem, ItemCategory, ItemRarity } from '.
 import { GlobalPointSystem } from '../services/GlobalPointSystem';
 import { useWallet } from '../contexts/WalletContext';
 import { Connection } from '@solana/web3.js';
-import PinkSugar from '../../assets/images/pink-sugar.png';
+import PinkSugar from '../../assets/images/Pink Sugar.png';
 import NovaEgg from '../../assets/images/nova-egg.png';
 import MiraBerry from '../../assets/images/mira-berry.png';
 import CloudCake from '../../assets/images/cloud-cake.png';
